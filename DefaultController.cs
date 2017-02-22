3e937305402b05e4876ab75edaba464cc54fdbee
zpf1989 <751682472@qq.com>
2017-02-22 17:13:46 +0800

    add

---
+++ DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace JexusOwin
+{
+    [AllowAnonymous]
+    public class DefaultController : ApiController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "hello Owin！";
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,27 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace JexusOwin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var httpConfig = new HttpConfiguration();
+            httpConfig.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+            //强制设定目前的WebApi返回格式为json
+            httpConfig.Formatters.Remove(httpConfig.Formatters.XmlFormatter);
+            //加载webapi中间件
+            app.UseWebApi(httpConfig);
+        }
+    }
+}
